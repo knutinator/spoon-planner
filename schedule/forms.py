37336393cb16288c64c9f4a56508d7fc2de63e8f
knutinator <jonatanknut@gmail.com>
2023-04-13 14:03:48 +0000

    Add validation to limit energy to max 100

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 from .models import DailyEnergy
 
 
 class DailyEnergyForm(forms.ModelForm):
+    user_energy = forms.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+
     class Meta:
         model = DailyEnergy
         fields = ['user_energy']
